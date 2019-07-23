801950eb5493df0eaf67d305cdd5d1708523b960
Miles Crighton <miles.crighton@gmail.com>
2019-07-23 16:44:21 +0100

    Added comments to API functions

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -7,27 +7,33 @@ const API_KEY = 'AIzaSyD6ba4mKmnnU0EVfg_hy_jNI3B8eJchAo4'
 export async function getID(username) {
     console.log('Getting ID...')
 
+    //Retrieve browser cached Ids
     let ids = JSON.parse(getLocalStorage('ytUserIDs'))
     if (ids === null) { ids = {} }
 
+    //If requested username is cached, return it
     if (ids[username] !== undefined) {
         let id = ids[username]
         console.log(`Found cached id: ${id} for ${username}`)
         return ids[username]
     }
 
-    const url = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername=' + username + '&key=' + API_KEY
-    let resp = await fetch(url)
+    //Request ID from YT API
+    const baseURL = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername='
+    const fullURL = baseURL + username + '&key=' + API_KEY
+    let resp = await fetch(fullURL)
     let json = await resp.json()
 
+    //If not found - throw error
     if (json.items.length === 0) {
         throw Error("Unable to convert username to ID");
     }
 
+    //Set ID from requested data
     let id = json.items[0].id
     console.log(`Id for ${username} fetched: ${id}`)
 
-    //Set local storage
+    //Store updated id map in browser
     ids[username] = id
     setLocalStorage('ytUserIDs', ids)
     console.log('IDs stored: ', getLocalStorage('ytUserIDs'))
@@ -43,9 +49,11 @@ export async function getID(username) {
 export async function getVideoDurations(videoIDs) {
     console.log('Getting video durations...')
     
+    //Retrieve browser cached durations
     let durations = JSON.parse(getLocalStorage('ytVideoDurations'))
     if (durations === null) { durations = {} }
 
+    //Generate string of video IDs that need to be requested
     let videoString = ''
     for (let i = 0; i < videoIDs.length; i++) {
         let id = videoIDs[i]
@@ -57,16 +65,19 @@ export async function getVideoDurations(videoIDs) {
         }
     }
 
+    //Fetch durations from YT API
     let baseURL = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails'
     let fullURL = baseURL + '&id=' + videoString + '&key=' + API_KEY 
     let resp = await fetch(fullURL)
     let json = await resp.json()
     let items = await json.items
 
+    //Add new durations to map
     for (let item of items) {
         durations[item.id] = convertVideoDuration(item.contentDetails.duration)
     }
 
+    //Store durations in browser
     setLocalStorage('ytUserIDs', durations)
     console.log('Durations stored: ', getLocalStorage('ytVideoDurations'))
 
